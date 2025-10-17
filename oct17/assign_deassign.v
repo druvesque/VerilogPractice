6064705d4f0885c6ab8f3ab14ea0b16b9d36d956
DhruvSachdeva <dhruvsachdeva6480@gmail.com>
2025-10-17 12:28:33 +0530

    assign

---
+++ oct17/assign_deassign.v
@@ -0,0 +1,42 @@
+module dflop(preset, clear, q, clk, d);
+    input preset, clear, clk, d;
+    output reg q;
+
+    always @(clear, preset) begin
+        if(!clear)
+            #10 assign q = 0;
+        else if(!preset)
+            #10 assign q = 1;
+        else
+            #10 deassign q;
+    end
+
+    always @(negedge clk) begin
+        #10 q <= d;
+    end
+endmodule
+
+module tb;
+    reg clk = 0;
+    reg preset = 1;
+    reg clear;
+    reg d;
+    wire q;
+
+    dflop d1(preset, clear, q, clk, d);
+
+    initial 
+        forever #10 clk = ~clk;
+
+    initial begin
+        d = 1;
+        clear = 0;
+        #10 clear = 1;
+        #10 d = 1'b0;
+    end
+
+    initial begin
+        $monitor("Time: %g, D: %b, Q: %b", $time, d, q);
+        #100 $finish;
+    end
+endmodule
